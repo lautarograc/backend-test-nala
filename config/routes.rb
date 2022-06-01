c6d323ea1e7a39e40e144a14249560151252d880
Fabián Prado <fabian@fabianprado.me>
2022-06-01 02:22:58 -0400

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   scope path: '/api' do
-api_version(module: 'Api::Exposed::V2', path: { value: 'v2' }, defaults: { format: 'json' }) do
-end    api_version(module: 'Api::Exposed::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
+    api_version(module: 'Api::Exposed::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
